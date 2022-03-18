d4c0a9ad0033efce3bbe37cb7007bcdba8e0c4c6
Anthony <info@anthonyscheeres.nl>
2022-03-18 17:24:49 +0100

    .

--- src/main/java/Ipsenh/xs/main/Program.java
+++ src/main/java/Ipsenh/xs/main/Program.java
@@ -1,5 +1,13 @@
 package Ipsenh.xs.main;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -11,13 +19,12 @@ public class Program {
 	   //schone installatie
 	   final String commandNormalCacheClean = "Remove-Item node_modules; yarn cache clean; time yarn install" ;
 	   
-
 	   
 	   
 //installeren aan de hand van een cache	   
 	   final String commandNormal = "yarn install; Remove-Item node_modules; time yarn install" ;
 
-	   
+	    
 		   
 		  //offline installatie aan de hand van een cache
 		   final String commandNormalOffline = "yarn install; Remove-Item node_modules; time yarn install --offline" ;
@@ -29,11 +36,12 @@ public class Program {
 
 		   String[] commands = { commandNormalCacheClean, commandNormal, commandNormalOffline, commandExistingNodeDependencies};
 		   
-		   
+		   List<String> resultz = new ArrayList<String>();
 	
 	   public Program (int iterations, double maxTemp) throws Exception {
 		   String[] ar = commands;
 		   for (int index2iteration = 0; index2iteration < iterations; index2iteration++){
+			   resultz = new ArrayList<String>();
 		    for (int index = 0; index < ar.length; index++) {
 		    	  
 	            // accessing each element of array
@@ -42,16 +50,63 @@ public class Program {
 	            if (sensor.isCPUTooHot()) {
 	            	throw new Exception("CPU is throttling");
 	            }
-	            String result = new CommandPowershell(commandx).getTime();
+	            String result2 = new CommandPowershell(createCurrentDirCommand(commandx)).getTime();
+	            
+	            resultz.add(result2);
+	         
+	            
 	        }
+		    
+		    resultzToResul();
+		    
 	   }
+		   String json = convertResultToJsonString();
+		   writeJson(json);
+	   }
+	
+	   private void resultzToResul() {
+		   result.SchoneInstallatie.add (resultz.get(0));
+		   result.InstallerenAanDeHandVanEenCache.add (resultz.get(1));
+		   result.OfflineInstallatieAanDeHandVanEenCache.add (resultz.get(2));
+		   result.InstallatieSchoneCache.add ( resultz.get(3));
 		   
 	   }
 	   
+	   private String createCurrentDirCommand(String command) {
+		   String COMMAND = "cd " + currentDir() + "; "+ command;
+		   return COMMAND;
+	   }
+	   
+	   private String currentDir() {
+		   String userDirectory = FileSystems.getDefault()
+			        .getPath("")
+			        .toAbsolutePath()
+			        .toString();
+		   return userDirectory;
+	   }
 	   
 	   
+	   private void writeJson(String json) {
+		   File dir = new File("tmp/json");
+		   dir.mkdirs();
+		   File tmp = new File(dir, "json.txt");
+		   try {
+			tmp.createNewFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		   
+		   try(BufferedWriter writer = new BufferedWriter(new FileWriter(tmp))) {
+		        writer.write(json);
+		    } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	   }
 	   
-	   private String convertToJsonString(){
+	   
+	   private String convertResultToJsonString(){
 
 		   ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 		   String json;
@@ -63,7 +118,7 @@ public class Program {
 			e.printStackTrace();
 		}
 
-		throw new Error("The coversion failed");
+		throw new Error("The conversion failed");
 		
 		   	}
 	   
