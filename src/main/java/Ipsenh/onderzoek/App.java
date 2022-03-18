79fb4e38ec0f427cf874a5f92453e5e400c598b3
Anthony <info@anthonyscheeres.nl>
2022-03-18 23:36:32 +0100

    .

--- src/main/java/Ipsenh/onderzoek/App.java
+++ src/main/java/Ipsenh/onderzoek/App.java
@@ -7,10 +7,12 @@ public class App {
     
     public static void main(String[] args) {
     	
+    	System.out.println("Hell orld program");
+    	
         //Set the timeout when waiting for command to terminate to 30 seconds instead of 10 (default value)
         Map<String, String> myConfig = new HashMap<>();
         myConfig.put("maxWait", args[1] );
-    	System.out.println(args[1]);
+    
 		   try {
 			Program program = new Program(Integer.parseInt(args[0]), 100 , myConfig);
 		} catch (Exception e) {
