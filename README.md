5f794f26f524f7f49dd6dfb986fee744d29d2658
Anthony <info@anthonyscheeres.nl>
2022-03-31 14:21:14 +0200

    generiek maken

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # onderzoek
 Een onderzoek naar de bijdrage van al verkregen resultaten voor het ontlasten van de pijplijnserver
+
+Voorbeeld commando's gebruikt voor onderzoek: 
+
+100 300000 3 npmInstall
+
+100 300000 3 yarnInstall
+
+100 300000 3 yarnInstallOffline
+
+
+
+Opties:
+
+Optie 1: iteraties of aantal keer dat het experiment wordt uitgevoerd
+
+Optie 2: timeout of tijd dat een commmand mag draaien
+
+Optie 3 pauze tussen commando's 
+
+Optie 4: npmInstall/yarnInstall/yarnInstallOffline 
--- src/main/java/Ipsenh/onderzoek/App.java
+++ src/main/java/Ipsenh/onderzoek/App.java
@@ -10,18 +10,29 @@ public class App {
     public static void main(String[] args) {
     	
     	
+   	  //offline installatie aan de hand van een cache
+  	   final String commandNormalOfflineNPM = 
+  			  
+  	   		  "Remove-Item node_modules -Recurse -Force -Confirm:$false; "
+  	   		+ "time { npm install --cache-min 9999999 --loglevel=error >$null 2>&1 }" ;
+  	   
+    	
+  	   
     	//time {yarn install -s >$null 2>&1 }
     	  //offline installatie aan de hand van een cache
  	   final String commandNormalOffline = 
  		
  	   		  "Remove-Item node_modules -Recurse -Force -Confirm:$false; "
  	   		+ "time { yarn install --offline -s >$null 2>&1 }" ;
+ 	  
+ 	   
  	   
  	  //offline installatie aan de hand van een cache
- 	   final String commandNormalOfflineNPM = 
- 			  
- 	   		  "Remove-Item node_modules -Recurse -Force -Confirm:$false; "
- 	   		+ "time { npm install --cache-min 9999999 --loglevel=error >$null 2>&1 }" ;
+ 	   final String commandNormalYarn = 
+
+  	   		  "yarn cache clean; "
+  	   		+ "time { yarn install -s >$null 2>&1 }" ;
+  	     
  	   
  	   
  	   
@@ -32,11 +43,37 @@ public class App {
         //Set the timeout when waiting for command to terminate to 30 seconds instead of 10 (default value)
         Map<String, String> myConfig = new HashMap<>();
         myConfig.put("maxWait", args[1] );
-        int timeout = Integer.parseInt(args[0]);
+        int iterations = Integer.parseInt(args[0]);
         int timeBetweenCommands =Integer.parseInt(args[2]) ;
 		   try { 
-			//new Program(timeout, 100 , myConfig, commandNormalOffline);
-			new Program(timeout, 100 , myConfig, commandNormalOfflineNPM, timeBetweenCommands );
+			String command = null;
+			   switch(args[3]) {
+			   case "yarnInstall":
+			     // code block
+				   command = commandNormalYarn;
+				   
+			     break;
+			   case "yarnInstallOffline":
+			     // code block
+				   command = commandNormalOffline;
+			     break;
+			     
+			     
+			   case "npmInstall":
+				   command = commandNormalOfflineNPM;
+				   break;
+			     
+			 }
+   
+			   
+			new Program(
+					iterations, 
+					100 , 
+					myConfig, 
+					command, 
+					timeBetweenCommands 
+					);
+			
 		} catch (Exception e) {
 			//do nothing lol
 		}
